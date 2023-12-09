ec795bf15319a635122c2d9856efac5e56dbf756
alonaranjo <alonso301090@gmail.com>
2023-12-09 12:18:09 -0600

    Ask a question form

--- app/(root)/ask-questions/page.tsx
+++ app/(root)/ask-questions/page.tsx
@@ -1,5 +1,14 @@
+import Question from "@/components/Forms/Question";
+
 function AskQuestions() {
-  return <div>Ask Questions</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 }
 
 export default AskQuestions;
