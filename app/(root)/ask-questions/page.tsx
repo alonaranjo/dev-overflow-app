5845c02dcb89e5bb26487b4148085d5a7d4d309e
alonaranjo <alonso301090@gmail.com>
2024-01-08 22:23:41 -0600

    trying to fix route ts

--- app/(root)/ask-questions/page.tsx
+++ app/(root)/ask-questions/page.tsx
@@ -1,11 +1,10 @@
 import Question from "@/components/Forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const AskQuestions = async () => {
   //const { userId } = auth();
-  const userId = "dummyClerk123";
+  const userId = "123456789";
   if (!userId) redirect("/sign-in");
   const mongoUser = await getUserById({ userId });
   return (
